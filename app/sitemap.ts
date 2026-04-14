0410a2730acaf095ba5db2f5e247993520051ba5
Diego Artola <diego@MacBook-Air-de-Diego.local>
2026-04-14 14:34:21 -0300

    Fix sitemap format

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,12 +1,30 @@
-import type { MetadataRoute } from "next";
+import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://garciastefano.com.ar",
+      url: "https://garciastefano.com.ar/",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
+    },
+    {
+      url: "https://garciastefano.com.ar/monotributo-argentina",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+    {
+      url: "https://garciastefano.com.ar/ingresos-brutos-arba",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+    {
+      url: "https://garciastefano.com.ar/responsable-inscripto-vs-monotributo",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
     },
   ];
 }
